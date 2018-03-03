4ad28920308c355dd9473f5f8995a4826ea7ce71
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-03-03 21:23:22 +0200

    make first test green as quick as possible

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      p1Name: '',
+      p2Name: '',
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <input onChange={el => this.setState({ p1Name: el.target.value })} data-hook="p1-input" />
+          <input onChange={el => this.setState({ p2Name: el.target.value })} data-hook="p2-input" />
+          <button data-hook="new-game">New Game</button>
+        </div>
+        <div>
+          <span data-hook="p1-name">{this.state.p1Name}</span>
+          <span data-hook="p2-name">{this.state.p2Name}</span>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
