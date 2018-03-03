71c03747bb9260d71168b81ce0ddcdf3e40c465f
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-03-03 11:36:44 +0200

    First red test

--- test/App.e2e.js
+++ test/App.e2e.js
@@ -1,5 +1,15 @@
-it('should have a title', async () => {
-  const page = await global.BROWSER.newPage();
-  await page.goto('http://localhost:3000');
-  expect(await page.title()).toBe('React App');
+describe('Tic Tac Toe', () => {
+  it('should start a new game', async () => {
+    const player1 = 'Yaniv';
+    const player2 = 'Computer';
+    const page = await global.BROWSER.newPage();
+    await page.goto('http://localhost:3000');
+    await page.type('[data-hook="p1-input"]', player1);
+    await page.type('[data-hook="p2-input"]', player2);
+    await page.click('[data-hook="new-game"]');
+    const p1Name = await page.$eval('[data-hook="p1-name"]', el => el.innerText);
+    const p2Name = await page.$eval('[data-hook="p2-name"]', el => el.innerText);
+    expect(p1Name).toBe(player1);
+    expect(p2Name).toBe(player2);
+  });
 });
