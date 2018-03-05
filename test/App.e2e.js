03b7e4b7933b51c7087d80565d10336b389e9735
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-03-06 00:03:22 +0200

    refactor driver to a file

--- test/App.e2e.js
+++ test/App.e2e.js
@@ -1,18 +1,4 @@
-const appDriver = page => ({
-  navigate: () => page.goto('http://localhost:3000'),
-  newGame: async (player1, player2) => {
-    await page.type('[data-hook="p1-input"]', player1);
-    await page.type('[data-hook="p2-input"]', player2);
-    await page.click('[data-hook="new-game"]');
-  },
-  getPlayer1Title: () => page.$eval('[data-hook="p1-name"]', el => el.innerText),
-  getPlayer2Title: () => page.$eval('[data-hook="p2-name"]', el => el.innerText),
-  clickACellAt: index => page.$$eval('[data-hook="cell"]', (cells, i) => cells[i].click(), index),
-  getACellValueAt: index =>
-    page.$$eval('[data-hook="cell"]', (cells, i) => cells[i].innerText, index),
-  getWinnerMessage: () => page.$eval('[data-hook="winner-message"]', el => el.innerText),
-  hasWinner: async () => !!await page.$('[data-hook="winner-message"]'),
-});
+const appDriver = require('./App.driver');
 
 describe('Tic Tac Toe', () => {
   let driver;
