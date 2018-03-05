72651dc8b6370f9638c298f8b1125e5608120291
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-03-05 22:34:42 +0200

    refactor test to drivers

--- test/App.e2e.js
+++ test/App.e2e.js
@@ -1,14 +1,30 @@
-describe('Tic Tac Toe', () => {
-  it('should start a new game', async () => {
-    const player1 = 'Yaniv';
-    const player2 = 'Computer';
-    const page = await global.BROWSER.newPage();
-    await page.goto('http://localhost:3000');
+const appDriver = page => ({
+  navigate: () => page.goto('http://localhost:3000'),
+  newGame: async (player1, player2) => {
     await page.type('[data-hook="p1-input"]', player1);
     await page.type('[data-hook="p2-input"]', player2);
     await page.click('[data-hook="new-game"]');
-    const p1Name = await page.$eval('[data-hook="p1-name"]', el => el.innerText);
-    const p2Name = await page.$eval('[data-hook="p2-name"]', el => el.innerText);
+  },
+  getPlayer1Title: () => page.$eval('[data-hook="p1-name"]', el => el.innerText),
+  getPlayer2Title: () => page.$eval('[data-hook="p2-name"]', el => el.innerText),
+});
+
+describe('Tic Tac Toe', () => {
+  let driver;
+  let page;
+
+  beforeEach(async () => {
+    page = await global.BROWSER.newPage();
+    driver = appDriver(page);
+    await driver.navigate();
+  });
+
+  it('should start a new game', async () => {
+    const player1 = 'Yaniv';
+    const player2 = 'Computer';
+    await driver.newGame(player1, player2);
+    const p1Name = await driver.getPlayer1Title();
+    const p2Name = await driver.getPlayer2Title();
     expect(p1Name).toBe(player1);
     expect(p2Name).toBe(player2);
   });
