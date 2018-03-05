e5b75cb10f8ee40064d43e00003ce474344a0536
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-03-05 22:52:46 +0200

    second red browser test

--- test/App.e2e.js
+++ test/App.e2e.js
@@ -7,6 +7,9 @@ const appDriver = page => ({
   },
   getPlayer1Title: () => page.$eval('[data-hook="p1-name"]', el => el.innerText),
   getPlayer2Title: () => page.$eval('[data-hook="p2-name"]', el => el.innerText),
+  clickACellAt: index => page.$$eval('[data-hook="cell"]', (cells, i) => cells[i].click(), index),
+  getACellValueAt: index =>
+    page.$$eval('[data-hook="cell"]', (cells, i) => cells[i].innerText, index),
 });
 
 describe('Tic Tac Toe', () => {
@@ -19,7 +22,7 @@ describe('Tic Tac Toe', () => {
     await driver.navigate();
   });
 
-  it('should start a new game', async () => {
+  test('should start a new game', async () => {
     const player1 = 'Yaniv';
     const player2 = 'Computer';
     await driver.newGame(player1, player2);
@@ -27,5 +30,13 @@ describe('Tic Tac Toe', () => {
     const p2Name = await driver.getPlayer2Title();
     expect(p1Name).toBe(player1);
     expect(p2Name).toBe(player2);
+  });
+
+  test('should show "X" after first player clicks', async () => {
+    const player1 = 'Yaniv';
+    const player2 = 'Computer';
+    await driver.newGame(player1, player2);
+    await driver.clickACellAt(0);
+    expect(await driver.getACellValueAt(0)).toBe('X');
   });
 });
