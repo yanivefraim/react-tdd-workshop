396dd8af68aa8d63fa6e2b59cf52ceb7bc3ecfc3
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-03-06 22:01:08 +0200

    add game logic (unit tests)

--- src/gameService.js
+++ src/gameService.js
@@ -1,9 +1,22 @@
 export const gameStatus = board => {
-  const isWin = symbol => board[0].every(cell => cell === symbol);
+  const isWin = symbol => {
+    const columns = board.reduce(
+      (acc, row) => row.map((cell, cellI) => [...(acc[cellI] || []), cell]),
+      [],
+    );
+    const diagonal1 = board.map((row, index) => row[index]);
+    const diagonal2 = board.map((row, index) => [...row].reverse()[index]);
+    return [...board, ...columns, diagonal1, diagonal2].some(row =>
+      row.every(cell => cell === symbol),
+    );
+  };
   if (isWin('X')) {
     return 'X';
   }
   if (isWin('O')) {
     return 'O';
+  }
+  if (board.every(row => row.every(cell => cell !== ''))) {
+    return '-';
   }
 };
