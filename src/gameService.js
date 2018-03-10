9085ababba56e40ce45123198bd9ffa1167a18ca
vsashyn <sashyn.v@gmail.com>
2018-03-10 15:05:56 +0200

    Add unit tests for scenarios: all rows, columns/diagonals/tie

--- src/gameService.js
+++ src/gameService.js
@@ -1,9 +1,39 @@
 export const gameStatus = board => {
-  const isWin = symbol => board[0].every(cell => cell === symbol);
-  if (isWin('X')) {
+  const isWinRow = symbol => board.some(row => row.every(cell => cell === symbol));
+  if (isWinRow('X')) {
     return 'X';
   }
-  if (isWin('O')) {
+  if (isWinRow('O')) {
     return 'O';
   }
+
+  const isWinColumn = symbol =>
+    board.every(row => row[0] === symbol) ||
+    board.every(row => row[1] === symbol) ||
+    board.every(row => row[2] === symbol);
+  if (isWinColumn('X')) {
+    return 'X';
+  }
+  if (isWinColumn('O')) {
+    return 'O';
+  }
+
+  const isWinDiagonally = symbol => board.every((row, ind) => row[ind] === symbol);
+  if (isWinDiagonally('X')) {
+    return 'X';
+  }
+  if (isWinDiagonally('O')) {
+    return 'O';
+  }
+
+  const isWinDiagonally2nd = symbol =>
+    board.every((row, ind, arr) => row[arr.length - ind - 1] === symbol);
+  if (isWinDiagonally2nd('X')) {
+    return 'X';
+  }
+  if (isWinDiagonally2nd('O')) {
+    return 'O';
+  }
+
+  return 'Tie';
 };
--- src/gameService.unit.js
+++ src/gameService.unit.js
@@ -1,6 +1,46 @@
 const { gameStatus } = require('./gameService');
 
-test('X should win', () => {
+test('X should win horizontaly (1 row)', () => {
   const board = [['X', 'X', 'X'], ['', '', ''], ['', '', '']];
   expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win horizontaly (2 row)', () => {
+  const board = [['', '', ''], ['X', 'X', 'X'], ['', '', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win horizontaly (3 row)', () => {
+  const board = [['', '', ''], ['', '', ''], ['X', 'X', 'X']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win vertically (1 column)', () => {
+  const board = [['X', '', ''], ['X', '', ''], ['X', '', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win vertically (2 column)', () => {
+  const board = [['', 'X', ''], ['', 'X', ''], ['', 'X', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win vertically (3 column)', () => {
+  const board = [['', 'X', ''], ['', 'X', ''], ['', 'X', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win diagonally', () => {
+  const board = [['X', '', ''], ['', 'X', ''], ['', '', 'X']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('X should win diagonally (2nd direction)', () => {
+  const board = [['', '', 'X'], ['', 'X', ''], ['X', '', '']];
+  expect(gameStatus(board)).toBe('X');
+});
+
+test('should be tie', () => {
+  const board = [['', '', 'X'], ['', '', 'X'], ['X', '', '']];
+  expect(gameStatus(board)).toBe('Tie');
 });
