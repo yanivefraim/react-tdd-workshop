77738a371df2b37f2af38aad29ec11299bd62805
vsashyn <sashyn.v@gmail.com>
2018-03-10 15:11:32 +0200

    Test verifiying a user cannot press a non empty cell

--- src/App.spec.js
+++ src/App.spec.js
@@ -38,3 +38,13 @@ test('"O" should win the game', () => {
   driver.clickACellAt(2);
   expect(driver.getWinnerMessage()).toBe(`${p2Name} won!`);
 });
+
+test('should not change value when click on non-empty cell', () => {
+  const p1Name = 'Yaniv';
+  const p2Name = 'Computer';
+  driver.render(<App />);
+  driver.newGame(p1Name, p2Name);
+  driver.clickACellAt(0);
+  driver.clickACellAt(0);
+  expect(driver.getACellAt(0)).toBe('X');
+});
