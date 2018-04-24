aac121421655d9c2364ebbff3502f7934bb47ec7
naor tedgi <naor2205@gmail.com>
2018-04-24 14:58:46 +0300

    step 1 complete logic and specs

--- src/gameService.js
+++ src/gameService.js
@@ -1,9 +1,44 @@
 export const gameStatus = board => {
-  const isWin = symbol => board[0].every(cell => cell === symbol);
-  if (isWin('X')) {
-    return 'X';
+  const line = (symbol, i) => board[i].every(cell => cell === symbol);
+
+  const winByline = symbol => {
+    return line(symbol, 0) || line(symbol, 1) || line(symbol, 2);
+  };
+
+  const winByRow = symbol => {
+    for (let i = 0; i < 3; i++) {
+      let l = board.map(e => e[i]);
+      if (l.every(cell => cell === symbol)) return true;
+    }
+    return false;
+  };
+
+  const winByDiagonal = symbol => {
+    let topRight =
+      board[0][0] === symbol &&
+      board[1][1] === symbol &&
+      board[2][2] === symbol;
+    let rightBottom =
+      board[0][2] === symbol &&
+      board[1][1] === symbol &&
+      board[2][0] === symbol;
+    return topRight || rightBottom;
+  };
+
+  const tie = ()=>{
+    board.forEach(element => {
+      let emptyCells =element.map(e => e ==='');
+      if (emptyCells.length > 1) return false;
+    });
+    return true
+  } 
+  
+  if (winByline("X") || winByRow("X") || winByDiagonal("X")) {
+    return "X";
   }
-  if (isWin('O')) {
-    return 'O';
+  if (winByline("O") || winByRow("O") || winByDiagonal("O")) {
+    return "O";
   }
+  if (tie())
+    return "tie"
 };
